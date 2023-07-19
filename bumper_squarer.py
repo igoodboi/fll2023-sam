c708f6cd97b0b18406d0ea69d6e6714f0e12d672
Samuel Wan <wanule123@gmail.com>
2023-07-18 20:57:25 -0700

    a bunch of files checked in in cluded bumpersquare

--- bumper_squarer.py
+++ bumper_squarer.py
@@ -2,30 +2,25 @@ from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, Motio
 from spike.control import wait_for_seconds, wait_until, Timer
 from math import *
 from spike.operator import equal_to
-# initalize
 hub = PrimeHub()
 wheels = MotorPair('A', 'B')
 leftboi = ColorSensor('F')
 rightboi = ColorSensor('E')
 hub.light_matrix.show_image('DUCK')
 
-def sensed_color():
-    return leftboi.get_reflected_light()<80 or rightboi.get_reflected_light()<80
-
-def color_equal():
-    return abs(leftboi.get_reflected_light() - rightboi.get_reflected_light())<3
-
+def squarer():
+    return leftboi.get_color()=='black' or rightboi.get_color()=='black'
+    
 def line_square(speed):
     wheels.start(0, speed)
-    wait_until(sensed_color)
-    print('maN')
+    wait_until(square)
     if leftboi.get_color()=='black':
-        wheels.start_tank(speed,5)
-        wait_until(color_equal)
+        wheels.start_tank(-5,speed)
+        wait_until(rightboi.get_color, equal_to,'black')
         wheels.stop()
     else:
-        wheels.start_tank(5,speed)
-        wait_until(color_equal)
+        wheels.start_tank(speed,-5)
+        wait_until(leftboi.get_color, equal_to,'black')
         wheels.stop()
 
-line_square(-10)
+line_square(10)
