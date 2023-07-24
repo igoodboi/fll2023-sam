1ad9d269026c6bd3465e4f629efd9fc5d44b78e6
Samuel Wan <wanule123@gmail.com>
2023-07-24 07:05:39 -0700

    add a pid squarer lightly tuned

--- pid_squarer.py
+++ pid_squarer.py
@@ -19,31 +19,31 @@ def line_square():
     while True:
         lsignal = leftboi.get_reflected_light() - target
         rsignal = rightboi.get_reflected_light() - target
-        if leftboi.get_color() == 'black' == rightboi.get_color() and  abs(lsignal-rsignal)<3 or count > 5:
+        if leftboi.get_color() != 'white' and rightboi.get_color() != 'white' and  (abs(lsignal-rsignal)<3 or count > 100):
             wheels.stop()
             break
         lspeed = gain(lsignal)
         rspeed = gain(rsignal)
-        if lsignal < 10 and rsignal < 10:
+        if lsignal < 3 and rsignal < 3:
             count += 1
             wheels.start_tank(lspeed // 3, rspeed // 3)
         else:
             wheels.start_tank(lspeed, rspeed)
-    print('Done')
+    print('Done squaring error = {} after {} tries'.format(lsignal - rsignal, count))
 
 
 def gain(signal):
     p = signal * .5
     # p is proportional feed back
-    d = (signal - history[-1]) * 0.
+    d = (signal - history[-1]) * 0.061
     # d = 0 cuz we dont need it
     history.append(signal)
-    if len(history) > 10:
+    if len(history) > 50:
         history.pop(0)
     # print(len(history))
-    i = sum(history) * 0.0
+    i = sum(history) * 0.05
     # return -int(p + i + d)
-    return -int(p)
+    return -int(p-d)
 
 history = [0]
 line_square()
