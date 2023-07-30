3b0bccfc5e3d5c55189133a7f5da59f58bf4e647
Shoupu Wan <swan@roblox.com>
2023-07-29 19:11:50 -0700

    check in one-eyed line follower

--- pybricks/line_follower_one_eye.py
+++ pybricks/line_follower_one_eye.py
@@ -11,42 +11,39 @@ rmotor = Motor(Port.A, positive_direction=Direction.COUNTERCLOCKWISE)
 wheels = DriveBase(left_motor=lmotor, right_motor=rmotor, wheel_diameter=56, axle_track=80)
 eye = ColorSensor(Port.C)
 eye.detectable_colors([Color.BLACK, Color.WHITE])
-goal = 40
-default_speed = -100
 
+goal = 40
+lowest = 20
+default_speed = -100
+p, i, d = 50, .3, 0
+
+len_hist = 20
+history = [goal]
+
+def gain(signal):
+    error = signal - goal
+    history.append(error)
+    if len(history) > len_hist:
+        history.pop(0)
+    return p * error + i * sum(history)
 
 def start_tank(left_speed, right_speed):
     speed = (right_speed + left_speed) / 2
     turn_rate = (right_speed - left_speed) / 80
     wheels.drive(speed, turn_rate)
-    pass
-
-
-gain = 70
 
 
 def line_follower(speed):
     # if u see black go 'drive' until u see white
-
     while True:
         c = eye.color()
         r = eye.reflection()
-        if c == Color.WHITE and r > 80:
-            wheels.drive(-10, 10)
-            wait(100)
-        elif c == Color.BLACK and r < 20:
-            wheels.drive(-10, -10)
-            wait(100)
-
-        diff = deviation() * gain
+        history.append(r)
+        if len(history) > 50:
+            history.pop(0)
+        signal = eye.reflection()
+        diff = gain(signal)
         start_tank(speed - diff, speed + diff)
-
-
-# when dieviated to left return value negative
-# when dieviated to right return value positive
-def deviation():
-    leftlight = eye.reflection()
-    return leftlight - goal
 
 
 # program
