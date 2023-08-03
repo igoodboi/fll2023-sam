e6c0e982bd7343a34d5c39d9039eb1c23df01d37
Shoupu Wan <swan@roblox.com>
2023-08-02 22:51:11 -0700

    a stabilizer taking care of tilt angle and angular veolocity

--- pybricks/stablizer.py
+++ pybricks/stablizer.py
@@ -9,35 +9,14 @@ lmotor = Motor(Port.B, positive_direction=Direction.COUNTERCLOCKWISE)
 rmotor = Motor(Port.A, positive_direction=Direction.CLOCKWISE)
 wheels = DriveBase(left_motor=lmotor, right_motor=rmotor, wheel_diameter=56, axle_track=80)
 
-
-class PID(object):
-    def __init__(self):
-        self.pid_gains = [50, .0, 0]
-        self.maxlen = 100
-        self.history = [0]
-        self.history_sum = 0
-
-    def get_response(self, signal):
-        p = self.pid_gains[0] * signal
-        history_diff = signal - self.history[-1]
-        d = self.pid_gains[2] * history_diff
-        self.history_sum += history_diff
-        i = self.pid_gains[1] * self.history_sum
-        self.history.append(signal)
-        if len(self.history) > self.maxlen:
-            self.history.pop(0)
-
-        # print('p = ' + str(p))
-        # print('i = ' + str(i))
-        # print('d = ' + str(d))
-        return p + i + d
+p, i, d = 60, 10, 0
 
 
 def stabilizer():
-    pid = PID()
     while True:
-        a = hub.imu.angular_velocity(Axis.X)
-        speed = pid.get_response(a)
+        v = hub.imu.angular_velocity(Axis.X)
+        _, a = hub.imu.tilt()
+        speed = v * p + a * i
         wheels.drive(speed, 0)
 
 
