bb5480adafe4352df7d0534f5d4ea76bca86186c
Samuel Wan <wanule123@gmail.com>
2023-08-19 20:03:10 -0700

    i did some more changes and dad helped me alot

--- pybricks/line_follower_one_eye_pid_sam.py
+++ pybricks/line_follower_one_eye_pid_sam.py
@@ -12,6 +12,7 @@ rmotor = Motor(Port.A, positive_direction=Direction.COUNTERCLOCKWISE)
 wheels = DriveBase(left_motor=lmotor, right_motor=rmotor, wheel_diameter=56, axle_track=80)
 eye = ColorSensor(Port.F)
 eye.detectable_colors([Color.BLACK, Color.WHITE])
+clock = StopWatch()
 margin = 1000
 slow_tingy = 70
 default_speed = 70
@@ -35,12 +36,12 @@ def gain(signal):
     return speed + diff, speed - diff
 
 
-def line_follower(distance=0,tim=0):
-    while wheels.distance() < distance:
+def line_follower(distance, time):
+    while wheels.distance() < distance and clock.time() < time:
         signal = eye.reflection()
         lspeed, rspeed = gain(signal)
         start_tank(lspeed, rspeed)
     wheels.stop()
 
 
-line_follower(distance=300)
+line_follower(300000, time=50000)
