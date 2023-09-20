b5e3316f5ee5f56ffe72a0c9f48523e50a8dad2f
Shoupu Wan <swan@roblox.com>
2023-09-20 15:56:37 -0700

    finish the backward. add debug mode

--- pybricks/fll2023/lib_polygon.py
+++ pybricks/fll2023/lib_polygon.py
@@ -4,6 +4,8 @@ from lib_move import move
 from lib_turn import turn
 from fll_robot import Robot
 
+debug = False
+
 
 def principle(angle):
     """
@@ -15,41 +17,45 @@ def principle(angle):
 
 
 # map size 2 m x 112 cm. let the 112 cm side be x; 2 m side be y
-def trip_plan(vertices, heading=0):
+def trip_plan(vertices, heading=0, motion_type=1):
     """
     Given a polygon vertices and initial heading, generate the trip plan (a sequence of maneuvers)
-    :param vertices:
-    :param heading:
-    :return:
+    :param vertices: the ordered vertices of the polygon, as a list of 2d coordinates, such as [(x1, y1), (x2, y2) ...], with the starting position as the first element
+    :param heading: starting heading direction (deg), follow pybricks orientation convention
+    :param motion_type: the direction for all the maneuvers: move forward = 1 (default), move backward = -1, freeze = 0 (turn only with no straight motion)
+    when motion_typ = 0 (freeze) turn angle is aimed at aligning the front of robot to the designated direction
+    :return: the maneuver sequence, e.g. [(turn_angle, distance, heading), ...]
     """
     maneuvers = []
     for i in range(1, len(vertices)):
-        turn_angle, distance, heading = route(vertices[i - 1], vertices[i], heading)
+        turn_angle, distance, heading = route(vertices[i - 1], vertices[i], heading, motion_type)
         maneuvers.append((turn_angle, distance, heading))
     return maneuvers
 
 
-def route(point1, point2, heading=0, reverse=False):
+def route(point1, point2, heading=0, motion_type=1):
     """
     Generate the routing parameters for a sequence of maneuvers, a turn plus a straight movement, that will ensure robot to traverse from point1 to point2.
     :param point1: starting point
     :param point2: ending point
     :param heading: starting heading direction (deg), follow pybricks orientation convention
-    :param reverse: boolean. if True, revert else drive forward (default)
+    :param motion_type: the direction for all the maneuvers: move forward = 1 (default), move backward = -1, freeze = 0 (turn only with no straight motion)
+    when motion_typ = 0 (freeze) turn angle is aimed at aligning the front of robot to the designated direction
     :return: a tuple of (turn angle, straight distance, ending orientation)
     """
     displacement = [point2[0] - point1[0], point2[1] - point1[1]]
     polar_angle = atan2(displacement[1], displacement[0]) * 180 / pi
     radius = sqrt(pow(displacement[1], 2) + pow(displacement[0], 2))
-    heading += 180 if reverse else 0
-    mod_angle = principle(polar_angle - heading)
-    distance = -radius if reverse else radius
-    print('original heading={} new heading={} dist={} turn={}'.format(
-        heading, polar_angle, radius, mod_angle))
-    return mod_angle, distance, polar_angle
+    new_heading = polar_angle + (180 if motion_type == -1 else 0)
+    turn_angle = principle(new_heading - heading)
+    distance = radius * motion_type
+    if debug:
+        print('original heading={} new heading={} dist={} turn={}'.format(
+            heading, new_heading, radius, turn_angle))
+    return turn_angle, distance, new_heading
 
 
-def poly(heading, vertices, robot: Robot, motion_type: int = 1, reverse: bool = False, speed=None):
+def polygon(heading, vertices, robot: Robot, motion_type: int = 1, reverse: bool = False, speed=None):
     """
     Given a polygon (vertices), plan the trip and execute the plan to traverse the polygon edges in order.
     heading = the initial orientation of the robot used for determining the first maneuver.
@@ -57,7 +63,7 @@ def poly(heading, vertices, robot: Robot, motion_type: int = 1, reverse: bool = 
     :param heading: initial heading direction (deg), follow pybricks orientation convention
     :param vertices: the ordered vertices of the polygon, as a list of 2d coordinates, such as [(x1, y1), (x2, y2) ...], with the starting position as the first element
     :param robot: the robot
-    :param motion_type: the direction for all the maneuvers: move forward = 1, move backward = -1, freeze = 0 (turn only with no straight motion)
+    :param motion_type: the direction for all the maneuvers: move forward = 1 (default), move backward = -1, freeze = 0 (turn only with no straight motion)
     :param reverse: if True, reverse course to undo all motions and retrack all the vertices back to the origin
     :param speed: specified speed for the whole trip. If unspecified (None), use system default
     :return: final heading (deg), follow pybricks orientation convention
@@ -65,11 +71,12 @@ def poly(heading, vertices, robot: Robot, motion_type: int = 1, reverse: bool = 
 
     def execute(maneuvers):
         for turn_angle, distance, _ in maneuvers:
-            print(turn_angle, distance * motion_type)
+            if debug:
+                print(turn_angle, distance)
             turn(robot, turn_angle)
-            move(robot, distance_mm=distance * motion_type, speed=speed)
+            move(robot, distance_mm=distance, speed=speed)
 
-    base_maneuvers = trip_plan(vertices, heading)
+    base_maneuvers = trip_plan(vertices, heading, motion_type)
     execute(base_maneuvers)
     if not reverse:
         return base_maneuvers[-1][-1]
@@ -82,11 +89,15 @@ def poly(heading, vertices, robot: Robot, motion_type: int = 1, reverse: bool = 
 
 if __name__ == "__main__":
     bot = Robot()
-    # heading = poly(0, [[0, 0], [100, 0]], bot, motion_type=1, reverse=True)
-    # heading = poly(0, [[0, 0], [100, 0]], bot, motion_type=-1, reverse=True)
-    heading = poly(180, [[0, 0], [100, 0]], bot, motion_type=-1, reverse=True)
-    # poly(0, [ [100, -100], [0, -100], [0, 0]], bot)
-    # poly(0, [[0, 0], [100, 0], [100, -100], [0, -100], [0, 0]], bot, reverse=True)
-    # heading = poly(0, [[0, 940], [500, 940], [700, 250], [760, 100]], bot)
-    # heading = poly(heading, [[760, 100], [700, 250]], bot, reverse=True)
-    # heading = poly(heading, [[700, 250], [600, 0], [660, 0], [700, 250], [500, 940], [0, 940]], bot)
+    debug = True
+    # heading = polygon(0, [[0, 0], [100, 0]], bot, motion_type=1, reverse=True)
+    # heading = polygon(0, [[0, 0], [100, 0]], bot, motion_type=-1, reverse=True)
+    # heading = polygon(180, [[0, 0], [100, 0]], bot, motion_type=-1)
+    # heading = polygon(180, [[0, 0], [100, 0]], bot, motion_type=-1, reverse=True)
+    # heading = polygon(0, [ [100, -100], [0, -100], [0, 0]], bot)
+    # heading = polygon(180, [ [100, -100], [0, -100], [0, 0]], bot)
+    # heading = polygon(0, [ [100, -100], [0, -100], [0, 0]], bot, motion_type=-1)
+    heading = polygon(0, [[0, 0], [100, 0], [100, -100], [0, -100], [0, 0]], bot, reverse=True)
+    # heading = polygon(0, [[0, 940], [500, 940], [700, 250], [760, 100]], bot)
+    # heading = polygon(heading, [[760, 100], [700, 250]], bot, reverse=True)
+    # heading = polygon(heading, [[700, 250], [600, 0], [660, 0], [700, 250], [500, 940], [0, 940]], bot)
